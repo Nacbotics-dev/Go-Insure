c103efbe38ef33fdb2db28d351fa3a0140976255
0xBitzz <successdaudu@gmail.com>
2023-07-14 16:22:39 +0100

    clean code

---
+++ backend/contract/deploy.py
@@ -0,0 +1,122 @@
+import json
+import os
+import shutil
+
+from algosdk import encoding
+from algosdk.atomic_transaction_composer import TransactionWithSigner
+from algosdk.transaction import PaymentTxn
+from beaker import client, consts, localnet
+from beaker.client.api_providers import AlgoNode, Network
+
+from go_insure import go_insure_app, purchase_policy, get_policy
+
+
+BUILD_PATH = "./artifacts"
+if os.path.exists(BUILD_PATH):
+    shutil.rmtree(BUILD_PATH)
+else:
+    import build
+    build.build()
+
+
+WEATHER_FEED_JSON_PATH = "./weather_feeds.json"
+
+
+def form_weather_json(area: str, state: str, country: str, date_time: str) -> None:
+    sign_key = "##signKey"
+    unit_of_measure = "metric"
+    weather_dict = {
+        "weather": {
+            "area": area,
+            "state": state,
+            "country": country,
+            "date_time": date_time,
+            "unit_of_measure": unit_of_measure,
+            "key": sign_key,
+        }
+    }
+    json_obj = json.dumps(weather_dict, indent=2)
+
+    with open(WEATHER_FEED_JSON_PATH, "w") as output_file:
+        output_file.write(json_obj)
+
+
+form_weather_json(
+    area="Ruiru", state="Kiambu", country="Kenya", date_time="2023-07-13T08:00:00"
+)
+
+
+def demo() -> None:
+    accts = localnet.get_accounts()
+
+    deployer_acct = accts[0]
+    acct1 = accts[1]
+    acct2 = accts[2]
+
+    algod_client = localnet.get_algod_client()
+
+    app_client = client.ApplicationClient(
+        client=algod_client,
+        app=go_insure_app,
+        sender=deployer_acct.address,
+        signer=deployer_acct.signer,
+    )
+    app_id, app_addr, _ = app_client.create()
+
+    print(f"Account 1 address: {acct1.address}")
+    print(f"Contract deployed with app id: {app_id}, and address: {app_addr}")
+
+    app_client.fund(10_000_000)
+
+    print(f"Deployer global state {app_client.get_global_state()}")
+
+    acct1_client = app_client.prepare(signer=acct1.signer)
+
+    # Account 1 - This approves
+    txn = PaymentTxn(
+        sender=acct1.address,
+        sp=algod_client.suggested_params(),
+        receiver=app_addr,
+        amt=consts.algo * 1,
+    )
+    acct1_client.call(
+        purchase_policy,
+        area="Ruiru",
+        state="Kiambu",
+        country="Kenya",
+        pay_txn=TransactionWithSigner(txn=txn, signer=acct1.signer),
+        boxes=[(app_id, encoding.decode_address(acct1.address))],
+    )
+    result = acct1_client.call(
+        get_policy,
+        addr=acct1.address,
+        boxes=[(app_id, encoding.decode_address(acct1.address))],
+    )
+    print(result.return_value)
+
+
+    # Account 2 - This rejects
+    txn = PaymentTxn(
+        sender=acct2.address,
+        sp=algod_client.suggested_params(),
+        receiver=app_addr,
+        amt=int(consts.algo * .3),
+    )
+    acct1_client.call(
+        purchase_policy,
+        area="Ruiru",
+        state="Kiambu",
+        country="Kenya",
+        pay_txn=TransactionWithSigner(txn=txn, signer=acct2.signer),
+        boxes=[(app_id, encoding.decode_address(acct2.address))],
+    )
+    result = acct1_client.call(
+        get_policy,
+        addr=acct2.address,
+        boxes=[(app_id, encoding.decode_address(acct2.address))],
+    )
+    print(result.return_value)
+
+
+if __name__ == "__main__":
+    demo()
---
+++ backend/contract/go_insure.py
@@ -0,0 +1,242 @@
+from beaker import Application, Authorize, BuildOptions, GlobalStateValue
+from beaker.consts import TRUE
+from beaker.lib.storage import BoxMapping
+from pyteal import (
+    Assert,
+    Balance,
+    Bytes,
+    Expr,
+    Global,
+    InnerTxnBuilder,
+    Int,
+    Seq,
+    TealType,
+    Txn,
+    TxnField,
+    TxnType,
+    abi,
+)
+
+PREMIUM_AMOUNT = Int(1_000_000)  # 1A
+COVERAGE_AMOUNT = Int(5_000_000)  # 5A
+
+DEFAULT_POLICY_EXPIRATION_TIMELINE = Int(31_536_000)
+
+PENDING = Bytes("Pending")
+APPROVED = Bytes("Approved")
+REJECTED = Bytes("Rejected")
+
+
+class Policy(abi.NamedTuple):
+    customer_address: abi.Field[abi.Address]
+    premium_amount: abi.Field[abi.Uint64]
+    active_status: abi.Field[abi.Bool]
+    registration_date: abi.Field[abi.Uint64]
+    expiration_date: abi.Field[abi.Uint64]  # 1yr from reg. date
+    claim_status: abi.Field[abi.String]
+    amount_claimed: abi.Field[abi.Uint64]
+    area: abi.Field[abi.String]
+    state: abi.Field[abi.String]
+    country: abi.Field[abi.String]
+
+
+class PolicyState:
+    insurer = GlobalStateValue(
+        stack_type=TealType.bytes, default=Bytes(""), descr="The insurer address"
+    )
+    coverage_amount = GlobalStateValue(
+        stack_type=TealType.uint64,
+        default=COVERAGE_AMOUNT,
+        descr="This is the coverage amount",
+    )
+    policy_expiration_date = GlobalStateValue(
+        stack_type=TealType.uint64,
+        default=DEFAULT_POLICY_EXPIRATION_TIMELINE,
+        descr="Expiration of policy. Default is set to yearly.",
+    )
+    address_to_policy = BoxMapping(abi.Address, Policy)
+
+
+go_insure_app = Application(
+    name="InsurancedApp",
+    descr="Insurance dApp",
+    state=PolicyState(),
+    build_options=BuildOptions(avm_version=8),
+)
+
+
+@go_insure_app.create
+def bootstrap() -> Expr:
+    """Bootstrap global state vlaues."""
+    return Seq(
+        go_insure_app.initialize_global_state(),
+        go_insure_app.state.insurer.set(Global.current_application_address()),
+    )
+
+
+@go_insure_app.external
+def purchase_policy(
+    pay_txn: abi.PaymentTransaction,
+    area: abi.String,
+    state: abi.String,
+    country: abi.String,
+) -> Expr:
+    """
+    Buys a new property policy for an account.
+    A new policy id is generated to identify the policy once purchased successfully.
+    """
+    txn = pay_txn.get()
+    return Seq(
+        Assert(
+            area.get() != Bytes(""),
+            state.get() != Bytes(""),
+            country.get() != Bytes(""),
+            comment="Invalid area, state or country",
+        ),
+        Assert(txn.sender() == Txn.sender(), comment="Sender mismatch"),
+        Assert(Global.group_size() == Int(2), comment="Group size not 2"),
+        Assert(
+            Balance(Txn.sender()) >= PREMIUM_AMOUNT,
+            comment="Sender balance is lower than Premium",
+        ),
+        Assert(
+            txn.type_enum() == TxnType.Payment,
+            txn.amount() == PREMIUM_AMOUNT,
+            txn.receiver() == go_insure_app.state.insurer.get(),
+            comment="Invalid txn type, amount or receiver.",
+        ),
+        (customer_address := abi.Address()).set(Txn.sender()),
+        (premium_amount := abi.Uint64()).set(PREMIUM_AMOUNT),
+        (active_state := abi.Bool()).set(TRUE),
+        (registration_date := abi.Uint64()).set(Global.latest_timestamp()),
+        (expiration_date := abi.Uint64()).set(
+            registration_date.get() + go_insure_app.state.policy_expiration_date.get()
+        ),
+        (claim_status := abi.String()).set(PENDING),
+        (amount_claimed := abi.Uint64()).set(Int(0)),
+        (policy := Policy()).set(
+            customer_address,
+            premium_amount,
+            active_state,
+            registration_date,
+            expiration_date,
+            claim_status,
+            amount_claimed,
+            area,
+            state,
+            country,
+        ),
+        go_insure_app.state.address_to_policy[Txn.sender()].set(policy),
+    )
+
+
+@go_insure_app.external
+def approve_claim() -> Expr:
+    """Approve policy claim"""
+    return Seq(
+        (policy := Policy()).decode(
+            go_insure_app.state.address_to_policy[Txn.sender()].get()
+        ),
+        (customer_address := abi.Address()).set(policy.customer_address),
+        (premium_amount := abi.Uint64()).set(policy.premium_amount),
+        (active_status := abi.Bool()).set(policy.active_status),
+        (registration_date := abi.Uint64()).set(policy.registration_date),
+        (expiration_date := abi.Uint64()).set(policy.expiration_date),
+        (claim_status := abi.String()).set(policy.claim_status),
+        (area := abi.String()).set(policy.area),
+        (state := abi.String()).set(policy.state),
+        (country := abi.String()).set(policy.country),
+        # Ensure the sender has already purchased a policy
+        Assert(customer_address.get() == Txn.sender()),
+        # Ensure PREMIUM amount has been paid
+        Assert(premium_amount.get() == PREMIUM_AMOUNT),
+        # Ensure policy is still active
+        Assert(active_status.get() == TRUE),
+        # Ensure claim status is still pending
+        Assert(claim_status.get() == PENDING),
+        # Issue payment
+        issue_payout(addr=customer_address.get(), amt=COVERAGE_AMOUNT),
+        # Set claim status to `APPROVED`
+        (claim_status := abi.String()).set(APPROVED),
+        (amount_claimed := abi.Uint64()).set(COVERAGE_AMOUNT),
+        policy.set(
+            customer_address,
+            premium_amount,
+            active_status,
+            registration_date,
+            expiration_date,
+            claim_status,
+            amount_claimed,
+            area,
+            state,
+            country,
+        ),
+        go_insure_app.state.address_to_policy[customer_address].set(policy),
+    )
+
+
+@go_insure_app.external
+def reject_claim() -> Expr:
+    """Reject a policy claim"""
+    return Seq(
+        (policy := Policy()).decode(
+            go_insure_app.state.address_to_policy[Txn.sender()].get()
+        ),
+        (customer_address := abi.Address()).set(policy.customer_address),
+        Assert(customer_address.get() == Txn.sender()),
+        (premium_amount := abi.Uint64()).set(policy.premium_amount),
+        (active_state := abi.Bool()).set(policy.active_status),
+        (registration_date := abi.Uint64()).set(policy.registration_date),
+        (expiration_date := abi.Uint64()).set(policy.expiration_date),
+        (claim_status := abi.String()).set(REJECTED),
+        (amount_claimed := abi.Uint64()).set(policy.amount_claimed),
+        (area := abi.String()).set(policy.area),
+        (state := abi.String()).set(policy.state),
+        (country := abi.String()).set(policy.country),
+        (policy := Policy()).set(
+            customer_address,
+            premium_amount,
+            active_state,
+            registration_date,
+            expiration_date,
+            claim_status,
+            amount_claimed,
+            area,
+            state,
+            country,
+        ),
+        go_insure_app.state.address_to_policy[customer_address].set(policy),
+    )
+
+
+def issue_payout(addr: abi.Address, amt: abi.Uint64) -> Expr:
+    """Issues coverage amount to the claiming customer"""
+    return Seq(
+        InnerTxnBuilder.Begin(),
+        InnerTxnBuilder.SetFields(
+            {
+                TxnField.type_enum: TxnType.Payment,
+                TxnField.receiver: addr,
+                TxnField.amount: amt,
+            }
+        ),
+        InnerTxnBuilder.Submit(),
+    )
+
+
+@go_insure_app.external
+def get_policy(addr: abi.Address, *, output: Policy) -> Expr:
+    """Gets policy for a specific address"""
+    return go_insure_app.state.address_to_policy[addr].store_into(output)
+
+
+@go_insure_app.external(authorize=Authorize.only_creator())
+def update_coverage_amount(amt: abi.Uint64) -> Expr:
+    """Update policy coverage amount."""
+    return go_insure_app.state.coverage_amount.set(amt.get())
+
+
+@go_insure_app.external(authorize=Authorize.only_creator())
+def update_expiration_timeline(expiration_timeline: abi.Uint64) -> Expr:
+    """Update policy expiration timeline."""
+    return go_insure_app.state.policy_expiration_date.set(expiration_timeline.get())
